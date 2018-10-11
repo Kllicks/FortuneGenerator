3d85d14d2d4ccd4d29b8f982b94ed93e24caa292
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-10-11 10:42:39 -0400

    adds fortunes that pop and setting to the div where it prints all the fortunes

--- scripts/index.js
+++ scripts/index.js
@@ -10,17 +10,33 @@ console.log(`this works`);
 
 // 1. make some code run when i click the button
 const triggerElement = document.querySelector(`[data-trigger]`);
+const outputElement = document.querySelector('[data-output]');
 
-function hello() {
-    console.log(`hello addEventListener!`);
-}
+const fortunes = [
+    "You laugh now, wait till you get home.",
+    "Fortune not found: Abor, Retry, Ignore?",
+    "About time I got out of that cookie.",
+    "Your resemblance to a muppet will prevent the world from taking you seriously", 
+    "You will be hungry again in one hour."
+];
+
+// function hello() {
+//     console.log(`hello addEventListener!`);
+// }
 
 // addEventListener accepts 2 arguments:
 // - a string that identifies the kind of event to listen for
 // - a function that says what to do when the event happens
-triggerElement.addEventListener(`click`, hello);
+triggerElement.addEventListener(`click`, function () {
+    console.log(`hello addEventListener!`);
+    const newFortune = fortunes.pop();
+    outputElement.textContent = newFortune;
+});
+// above is an anonymous function, which you should use if you're only using it once
 
 // 2. Write some text to the page on button click
+
+
 
 
 // 3. pull text from an array of text to write to the page
