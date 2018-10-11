64d5fea5ee5fdd97a7298bd7ff863bfd5b0b2078
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-10-11 11:53:52 -0400

    adds a second button and makes both print from an array list randomly

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-console.log(`this works`);
+// console.log(`this works`);
 // don't use ID's because they're error prone
 // Classes are for CSS Styling
 // "data attributes" are for JS
@@ -6,10 +6,9 @@ console.log(`this works`);
     // data attributes are good because if someone is changing the CSS classes it wont affect your JS
     // classes describe the location, data attributes describe the action
 
-
-
 // 1. make some code run when i click the button
 const triggerElement = document.querySelector(`[data-trigger]`);
+const triggerElement2 = document.querySelector(`[data-trigger2]`);
 const outputElement = document.querySelector('[data-output]');
 
 const fortunes = [
@@ -19,24 +18,47 @@ const fortunes = [
     "Your resemblance to a muppet will prevent the world from taking you seriously", 
     "You will be hungry again in one hour."
 ];
-
-// function hello() {
-//     console.log(`hello addEventListener!`);
-// }
+const nflTeams = [
+    "Houston Texans",
+    "Atlanta Falcons",
+    "New Orleans Saints",
+    "New York Giants",
+    "Kansas City Chiefts"
+]
+// let index = 0;
 
 // addEventListener accepts 2 arguments:
 // - a string that identifies the kind of event to listen for
 // - a function that says what to do when the event happens
+
+//var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
 triggerElement.addEventListener(`click`, function () {
-    console.log(`hello addEventListener!`);
-    const newFortune = fortunes.pop();
+    console.log(`fortune`);
+    // const newFortune = fortunes.pop();
+    // const newFortune = fortunes[index];
+    const newFortune = fortunes[Math.floor(Math.random()*fortunes.length)];
     outputElement.textContent = newFortune;
+    // index++;
+    // if (index > (fortunes.length - 1)){
+    //     index = 0;
+    // }
+    
 });
-// above is an anonymous function, which you should use if you're only using it once
 
-// 2. Write some text to the page on button click
+triggerElement2.addEventListener(`click`, function() {
+    console.log(`NFL`);
+    const newTeam = nflTeams[Math.floor(Math.random()*nflTeams.length)];
+    outputElement.textContent = newTeam;
+});
 
+// function triggerEvent (obj) {
+//     const newObj = obj[index];
+//     outputElement.textContent = newObj;
+//     index++;
+//     if (index > (obj.length - 1)){
+//         index = 0;
+//     }
+// }
 
-
-
-// 3. pull text from an array of text to write to the page
+// triggerElement.addEventListener(`click`, triggerEvent(fortunes));
+// triggerElement2.addEventListener(`click`, triggerEvent(nflTeams));
