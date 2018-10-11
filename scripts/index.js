a69fa824c4f006c30ad7f806e92717a4b76bf765
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-10-11 09:36:28 -0400

    adds comments and data attribute

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+console.log(`this works`);
+// don't use ID's because they're error prone
+// Classes are for CSS Styling
+// "data attributes" are for JS
+    // they always start with "data-"
+    // data attributes are good because if someone is changing the CSS classes it wont affect your JS
+    // classes describe the location, data attributes describe the action
+
+
+
+// 1. make some code run when i click the button
+
+
+// 2. Write some text to the page on button click
+
+
+// 3. pull text from an array of text to write to the page
